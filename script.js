2df9a8198712d319bfaf1b9e67aabe2675c08c9a
aenand15 <amitenand@gmail.com>
2018-12-10 23:52:46 -0500

    img not displaying

---
+++ script.js
@@ -0,0 +1,316 @@
+var root="http://comp426.cs.unc.edu:3001/",
+user="pacquito",
+pass="superiorte4l";
+var portIds='';
+var names = []
+
+function getAportIdsWrapper(){
+    getFlights();
+    return true;
+}
+
+function start(){
+    login()
+    return true;
+}
+function login(){
+    $.ajax(root+'sessions',{
+    type: 'POST',
+    dataType: 'json',
+    data:{
+        "user":{
+            "username": "pacquito",
+            "password": "superiorte4l"
+        }
+    },
+    xhrFields: {withCredentials: true},
+    success: (response, status) =>{
+        //nothing needs to happen here other than 'calling the login function' at some point
+        console.log('Success');
+    }, error: () =>{
+        console.log('error');
+    }
+    });
+}
+
+function getAirlines(){
+    $.ajax(root+'airlines', {
+        type: 'GET',
+        dataType: 'JSON',
+        xhrFields: {withCredentials: true},
+        success: (response) =>{
+            console.log(response);
+        }, error: () =>{
+            console.log('error');
+        }
+    });
+}
+
+function getFlights(){
+    $.ajax(root+'flights', {
+        type: 'GET',
+        dataTpe: 'json',
+        xhrFields: {withCredentials: true},
+        success: (response) =>{
+        //for loop generates 10 random numbers to gain flight information for
+            var rando=[]
+            for(let i=0; i<4;i++){
+                let rng = Math.floor(Math.random() * response.length)
+                if(!rando.includes(rng)){
+                rando.push(rng)
+                } else {
+                    i--;}
+            }
+            //console.log(rando);
+            //this for loop takes a random flight from the rng and grabs its airport ids
+            var aports = []
+            var dports = []
+            for(let j=0; j<rando.length; j++){
+                aports.push(response[rando[j]].arrival_id)
+                dports.push(response[rando[j]].departure_id)
+            }
+            portIds=dports[0] + " " + aports[0];
+            getAirport(aports[0])
+            getAirport(aports[1])
+            getAirport(aports[2])
+            getAirport(aports[3])
+            return aports;
+        }, error: ()  =>{
+            console.log('error');
+        }
+    });
+}
+var airports =[]
+//call allAirports to get the airports array so user can select their airport
+function allAirports(){
+    $.ajax(root+'airports',{
+        type: 'GET',
+        dataType: 'json',
+        xhrFields: {withCredentials: true},
+        success: (response) =>{
+            //create a list that holds all airports and their ids
+                //so user can look up airport ids for flight creation
+            for(let i=0;i<response.length; i++){
+                airports.push('(airport_id:'+ response[i].id+')'+' '+response[i].name)
+            }
+            //console.log(airports);
+            return airports;
+        }, error: () =>{
+            console.log('error');
+        }
+    });
+}
+//taking airport_id, returns airport name and city
+function getAirport(getting_carded){
+    //getting_carded is the airport id
+    $.ajax(root+'airports/'+getting_carded,{
+        type: 'GET',
+        dataType: 'json',
+        xhrFields: {withCredentials: true},
+        success: (response) =>{
+            //response.name is the airport name
+            console.log(response.name+' in: '+response.city);
+            names.push(response.city)
+            return response.city
+        }, error: () =>{
+            console.log('error');
+        }
+    });
+}
+
+//assuming a 'form' submission with airports and times (flight number is also required)
+function createFlight(){
+    //read form fields with these ids
+    var departFrom = $('#dPort').val(),
+    arriveAt = $('#aPort').val(),
+    dTime = $('#departureTime').val(),
+    aTime = $('#arrivalTime').val(),
+    flNum = $('#flightNum').val();
+    $.ajax(root+'flights',{
+        type: 'POST',
+        dataType: 'json',
+        xhrFields: {withCredentials: true},
+        data:{
+            "flight":{
+                "departs_at": dTime,
+                "arrives_at": aTime,
+                "number": flNum,
+                "departure_id": departFrom,
+                "arrival_id": arriveAt
+            }
+        },
+        success: (response) =>{
+            console.log('New flight created');
+            portIds = departFrom +' '+arriveAt;
+            console.log(portIds);
+            return portIds;
+        }
+    })
+}
+
+function startsc(){
+    var query = '';
+    var serp =[], serp2=[];
+    var user ='';
+    $.ajax(root+'airports/'+portIds.substring(7),{
+        type: 'GET',
+        dataType: 'json',
+        xhrFields: {withCredentials: true},
+        success: (response) =>{
+            //response.name is the airport name
+            //console.log(response.name+' in: '+response.city);
+            query = response.city
+            //console.log(query);
+        
+                const hash = window.location.hash.substring(1).split('&').reduce(function (initial, item) {
+                    if (item) {
+                        var parts = item.split('=');
+                        initial[parts[0]] = decodeURIComponent(parts[1]);
+                    }
+                    return initial;
+                    }, {});
+
+                window.location.hash = '';
+
+                // Set token
+                let _token = hash.access_token;
+
+                let authEndpoint = 'https://accounts.spotify.com/authorize';
+
+                // Replace with your app's client ID, redirect URI and desired scopes
+                let clientId = '7bf8e705b8784dc093df8333cc8fea87';
+                let redirectUri = 'http://www.cs.unc.edu/Courses/comp426-f18/users/iankdew/FP/test.html';
+                let scopes = [
+                'playlist-modify-public user-read-private user-read-birthdate'];
+
+                // If there is no token, redirect to Spotify authorization
+                if (!_token) {
+                window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=playlist-modify-public user-read-private user-read-birthdate&response_type=token&show_dialog=true`;
+                }
+                // Make a call using the token
+                
+                $.ajax({
+                url: "https://api.spotify.com/v1/search",
+                type: "GET",
+                beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+                dataType: 'json',
+                data:{
+                    q: query,
+                    type: 'track',
+                    market: 'US',
+                    limit: 20
+                },
+                success: function(resdata) { 
+                        for(let i=0; i<resdata.tracks.items.length; i++){
+                            //item is a uri we can place into the spotify iframe widget
+                        let item = $('<li>'+resdata.tracks.items[i].uri.substring(8)+'</li>');
+                        item.appendTo($('#top-artists'));
+                        serp.push(resdata.tracks.items[i].uri.substring(8).replace(':','/'));
+                        serp2.push(resdata.tracks.items[i].uri);
+                        }
+                    // Do something with the returned data
+                    //call play widget with item as parameter
+                    $.ajax('https://api.spotify.com/v1/me',{
+                    type:'GET',
+                    dataType:'json',
+                    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer '+_token);},
+                    success: (response) =>{
+                        user = response.id;
+                        //console.log(_token);
+                        createPlayList(query, serp,serp2, _token, user);
+
+                    }
+                })
+                },
+                });
+            }, error: () =>{
+                console.log('error');
+            }
+        });
+}
+var newRP;
+//create playlist function
+    //from the search results create a playlist. 
+function createPlayList(qry, rp,rp2, tk, usr){
+     console.log(qry);
+    // console.log(rp);
+     //console.log(tk);
+    // console.log(usr);
+    randoRP=[];
+    for(let i=0; i<10;i++){
+        let rng = Math.floor(Math.random() * 10)
+        if(!randoRP.includes(rng)){
+        randoRP.push(rng)
+        } else {
+            i--;}
+    }
+    //newRP is a *shuffled* tracklist order from the results. use newRP to add track links to the playlist
+    //var newRP = []
+        $.ajax('https://api.spotify.com/v1/users/'+usr+'/playlists',{
+        type: 'POST',
+        dataType: 'json',
+        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer '+tk);},
+        contentType: 'application-json',
+        data: JSON.stringify({
+            'name': qry}),
+        success: (response) =>{
+                    var plID = response.id;
+                    var plUri = response.uri;
+                    for(let j=0;j<randoRP.length;j++){
+                        var newRP = rp[randoRP[j]]}
+                    
+                        $.ajax('https://api.spotify.com/v1/playlists/'+plID+'/tracks?uris='+rp2[0],{
+                            type: 'POST',
+                            dataType: 'json',
+                            beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer '+tk);},
+                            contentType: 'application-json',
+                            success: (response) =>{
+                                playList(plUri)
+                            }, error: (response) =>{
+                                console.log(rp2[0]);
+                            }
+                        })
+                    
+            //playList(plID, plUri);
+        }, error: (response) => {
+            console.log(response.responseText);
+        }
+    })
+}
+function playList(uri){
+    var url = uri.substring(39).replace(':','/');
+    console.log(url);
+    var playBtn = $('<iframe id= "play" src="https://open.spotify.com/embed/'+url+'" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+    var play_goes_here = $('#playgoeshere');
+    playBtn.appendTo(play_goes_here);
+}
+//playList grabs the play widget and plays the newly created playlist.
+
+var buildGateInterface = function(){
+    console.log(names)
+    let container = $("<div class = 'container'></div>");
+    container.append("<h1 id = 'headerTitle'>Welcome!</h1>")
+    container.append('<h2>Click where you want to go!<h2>')
+    let bar = $("<div class = 'slideshow'></div>")
+    let numtodo = 4
+    for (i = 1; i <= numtodo; i++){
+        let s = 'pic' + i
+        let z = 'slide'+i
+        let city = names[i-1]
+        let url = "https://source.unsplash.com/800x400/?" + city + "?sig=" + i
+        
+        fetch(url).then(function(response) {
+            bar.append("<div class = 'slide' id = " + z  + 
+            "><img alt = 'testing' id = " + s + " src = " +response.url+
+            " ><span>" + city + "</span><button class = 'fly' onclick = 'buildFlightInterface(" + z + ")'>Fly Here</button></div>")
+      });
+    }
+    container.append(bar)
+    let body = $('body')
+    body.empty()
+    body.append(container)
+}
+var buildFlightInterface = function(id){
+    let city = $(id).find('span').text()
+    console.log(city)
+}
--- slider.js
+++ slider.js
@@ -1,4 +1,7 @@
-
+$(document).ready(() =>{
+    start();
+    getAportIdsWrapper();
+});
 function sleep(milliseconds) {
     let start = new Date().getTime();
     for (let i = 0; i < 1e7; i++) {
@@ -11,7 +14,7 @@ function progressBar(){
     $('#bar').append('Going through TSA Security...')
     let elem = document.getElementById("bar");   
     let width = 19;
-    let id = setInterval(frame, 50);
+    let id = setInterval(frame, 100);
     function frame() {
         if (width >= 100) {
             clearInterval(id)
@@ -21,31 +24,4 @@ function progressBar(){
             elem.style.width = width + '%'; 
         }
     }
-}
-
-var buildGateInterface = function(){
-    let container = $("<div class = 'container'></div>");
-    container.append("<h1 id = 'headerTitle'>Welcome!</h1>")
-    container.append('<h2>Click where you want to go!<h2>')
-    let bar = $("<div class = 'slideshow'></div>")
-    let numtodo = 4
-    //city = getPossibleFlights()
-    for (i = 1; i <= numtodo; i++){
-        let s = 'pic' + i
-        let z = 'slide'+i
-        let url = "https://source.unsplash.com/800x400/?london?sig=" + i
-        fetch(url).then(function(response) {
-            bar.append("<div class = 'slide' id = " + z  + 
-            "><img alt = 'testing' id = " + s + " src = " +response.url+
-            " ><span>London</span><button class = 'fly' onclick = 'buildFlightInterface(" + z + ")'>Fly Here</button></div>")
-      });
-    }
-    container.append(bar)
-    let body = $('body')
-    body.empty()
-    body.append(container)
-}
-var buildFlightInterface = function(id){
-    let city = $(id).find('span').text()
-    console.log(city)
 }
